42a5b450246630b0b18a0ac43ccf3e9df00c5789
Junior Dugue <jrdugue@gmail.com>
2019-02-19 14:12:56 -0500

    added basic html structure for the game via js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,22 @@ window.addEventListener("load",init);
 
 function init(){
   console.log("ready");
-  
+
+  let div = document.createElement("div");
+
+  div.setAttribute("class", "message");
+
+  div.innerText = "press start button";
+  document.body.appendChild(div);
+
+  let button = document.createElement("button");
+
+  button.type = "button";
+  button.innerText = "Start Game";
+  document.body.appendChild(button);
+
+  let div1 = document.createElement("div");
+  div1.classList.add("game");
+
+  document.body.appendChild(div1);
 }
